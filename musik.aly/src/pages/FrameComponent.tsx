d5a51675ac71a88489282a6d64a11468801ec4d7
timibash <85651144+timibash@users.noreply.github.com>
2022-12-09 22:47:10 +0000

    Add files via upload

---
+++ musik.aly/src/pages/FrameComponent.tsx
@@ -0,0 +1,416 @@
+import { FunctionComponent } from "react";
+import styles from "./FrameComponent.module.css";
+
+const FrameComponent: FunctionComponent = () => {
+  return (
+    <div className={styles.frameDiv}>
+      <div className={styles.frameDiv1} />
+      <div className={styles.frameDiv1} />
+      <div className={styles.desktop1}>
+        <div className={styles.rectangleDiv} />
+        <div className={styles.rectangleDiv1} />
+        <div className={styles.groupDiv}>
+          <div className={styles.rectangleDiv2} />
+          <div className={styles.rectangleDiv3} />
+          <div className={styles.rectangleDiv4} />
+          <div className={styles.rectangleDiv5} />
+        </div>
+        <div className={styles.rectangleDiv6} />
+        <div className={styles.groupDiv1}>
+          <div className={styles.rectangleDiv7} />
+          <div className={styles.rectangleDiv8} />
+          <div className={styles.rectangleDiv9} />
+          <div className={styles.rectangleDiv10} />
+          <div className={styles.rectangleDiv11} />
+        </div>
+        <div className={styles.groupDiv2}>
+          <div className={styles.rectangleDiv7} />
+        </div>
+        <div className={styles.groupDiv3}>
+          <img className={styles.frameIcon} alt="" src="../frame.svg" />
+          <b className={styles.musikalyB}>Musik.aly</b>
+        </div>
+        <b className={styles.musicB}>Music</b>
+        <b className={styles.yourTop20}>Your Top 20</b>
+        <b className={styles.songsYouMightLike}>Songs you might like</b>
+        <b className={styles.recentlyPlayedB}>Recently played</b>
+        <b className={styles.seeAllB}>See all</b>
+        <b className={styles.seeAllB1}>See all</b>
+        <b className={styles.seeAllB2}>See all</b>
+        <b className={styles.notificationsB}>Notifications</b>
+        <b className={styles.podcastB}>Podcast</b>
+        <b className={styles.liveB}>Live</b>
+        <b className={styles.radioB}>Radio</b>
+        <img className={styles.groupIcon} alt="" src="../group-5.svg" />
+        <img className={styles.groupIcon1} alt="" src="../group-5.svg" />
+        <img className={styles.groupIcon2} alt="" src="../group-5.svg" />
+        <img className={styles.groupIcon3} alt="" />
+        <div className={styles.mENUDiv}>MENU</div>
+        <div className={styles.groupDiv4}>
+          <b className={styles.musikalyB}>Search</b>
+          <img
+            className={styles.iconlyBoldSearch}
+            alt=""
+            src="../iconlyboldsearch.svg"
+          />
+        </div>
+        <div className={styles.groupDiv5}>
+          <b className={styles.musikalyB}>Home</b>
+          <img
+            className={styles.iconlyBoldHome}
+            alt=""
+            src="../iconlyboldhome.svg"
+          />
+        </div>
+        <div className={styles.rectangleDiv13} />
+        <div className={styles.groupDiv6}>
+          <b className={styles.artistsB}>Artists</b>
+          <img
+            className={styles.iconlyBoldProfile}
+            alt=""
+            src="../iconlyboldprofile.svg"
+          />
+        </div>
+        <div className={styles.groupDiv7}>
+          <b className={styles.artistsB}>Albums</b>
+          <img
+            className={styles.iconlyBoldBookmark}
+            alt=""
+            src="../iconlyboldbookmark.svg"
+          />
+        </div>
+        <div className={styles.groupDiv8}>
+          <b className={styles.musikalyB}>Discover</b>
+          <img
+            className={styles.iconlyBoldDiscovery}
+            alt=""
+            src="../iconlybolddiscovery.svg"
+          />
+        </div>
+        <div className={styles.lIBRARYDiv}>LIBRARY</div>
+        <div className={styles.groupDiv9}>
+          <b className={styles.musikalyB}>Recent</b>
+          <img
+            className={styles.iconlyBoldActivity}
+            alt=""
+            src="../iconlyboldactivity.svg"
+          />
+        </div>
+        <div className={styles.groupDiv10}>
+          <b className={styles.musikalyB}>Favorites</b>
+          <img
+            className={styles.iconlyBoldHeart}
+            alt=""
+            src="../iconlyboldheart.svg"
+          />
+        </div>
+        <div className={styles.groupDiv11}>
+          <b className={styles.localB}>Local</b>
+          <img
+            className={styles.iconlyBoldPaperNegative}
+            alt=""
+            src="../iconlyboldpaper-negative.svg"
+          />
+        </div>
+        <div className={styles.pLAYLISTDiv}>PLAYLIST</div>
+        <div className={styles.groupDiv12}>
+          <b className={styles.localB}>Create new</b>
+          <img
+            className={styles.iconlyBoldPaperPlus}
+            alt=""
+            src="../iconlyboldpaper-plus.svg"
+          />
+        </div>
+        <div className={styles.groupDiv13}>
+          <b className={styles.musikalyB}>Afro pop</b>
+          <img
+            className={styles.iconlyBoldPaper}
+            alt=""
+            src="../iconlyboldpaper.svg"
+          />
+        </div>
+        <div className={styles.groupDiv14}>
+          <b className={styles.musikalyB}>World</b>
+          <img
+            className={styles.iconlyBoldPaper1}
+            alt=""
+            src="../iconlyboldpaper.svg"
+          />
+        </div>
+        <div className={styles.gENERALDiv}>GENERAL</div>
+        <div className={styles.groupDiv15}>
+          <b className={styles.localB}>Settings</b>
+          <img
+            className={styles.iconlyBoldSetting}
+            alt=""
+            src="../iconlyboldsetting.svg"
+          />
+        </div>
+        <div className={styles.groupDiv16}>
+          <b className={styles.localB}>{`Log Out `}</b>
+          <img
+            className={styles.iconlyBoldLogout}
+            alt=""
+            src="../iconlyboldlogout.svg"
+          />
+        </div>
+        <div className={styles.groupDiv17}>
+          <b className={styles.endureB}>Endure</b>
+          <div className={styles.celineDiv}>Celine</div>
+          <div className={styles.groupDiv18}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="../rectangle-12@2x.png"
+            />
+          </div>
+          <img className={styles.frameIcon1} alt="" src="../frame1.svg" />
+        </div>
+        <b className={styles.bootyBounce}>Booty bounce</b>
+        <div className={styles.remaDiv}>Rema</div>
+        <div className={styles.groupDiv19}>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="../rectangle-121@2x.png"
+          />
+        </div>
+        <img className={styles.frameIcon2} alt="" src="../frame2.svg" />
+        <div className={styles.groupDiv20}>
+          <b className={styles.endureB}>Beats</b>
+          <div className={styles.celineDiv}>AJR</div>
+          <div className={styles.groupDiv18}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="../rectangle-122@2x.png"
+            />
+          </div>
+          <img className={styles.frameIcon3} alt="" src="../frame3.svg" />
+        </div>
+        <div className={styles.groupDiv22}>
+          <b className={styles.endureB}>Focus</b>
+          <div className={styles.celineDiv}>Joeboy</div>
+          <div className={styles.groupDiv18}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="../rectangle-123@2x.png"
+            />
+          </div>
+          <img className={styles.frameIcon3} alt="" src="../frame4.svg" />
+        </div>
+        <div className={styles.groupDiv24}>
+          <b className={styles.endureB}>Sex</b>
+          <div className={styles.celineDiv}>Trey</div>
+          <div className={styles.groupDiv18}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="../rectangle-124@2x.png"
+            />
+          </div>
+          <img className={styles.frameIcon3} alt="" src="../frame5.svg" />
+        </div>
+        <b className={styles.b}>23</b>
+        <div className={styles.burnaBoyDiv}>Burna boy</div>
+        <div className={styles.groupDiv26}>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="../rectangle-125@2x.png"
+          />
+        </div>
+        <img className={styles.frameIcon6} alt="" src="../frame6.svg" />
+        <div className={styles.groupDiv27}>
+          <b className={styles.endureB}>King kunta</b>
+          <div className={styles.celineDiv}>Kendrick lamar</div>
+          <div className={styles.groupDiv18}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="../rectangle-126@2x.png"
+            />
+          </div>
+          <img className={styles.frameIcon7} alt="" src="../frame7.svg" />
+        </div>
+        <div className={styles.groupDiv29}>
+          <b className={styles.endureB}>Dancers</b>
+          <div className={styles.celineDiv}>The crypt</div>
+          <div className={styles.groupDiv18}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="../rectangle-127@2x.png"
+            />
+          </div>
+          <img className={styles.frameIcon3} alt="" src="../frame8.svg" />
+        </div>
+        <b className={styles.remaB}>Rema</b>
+        <div className={styles.groupDiv31}>
+          <img
+            className={styles.rectangleIcon8}
+            alt=""
+            src="../rectangle-128@2x.png"
+          />
+        </div>
+        <b className={styles.fireboy}>Fireboy</b>
+        <div className={styles.groupDiv32}>
+          <img
+            className={styles.rectangleIcon8}
+            alt=""
+            src="../rectangle-129@2x.png"
+          />
+        </div>
+        <b className={styles.joeboy}>Joeboy</b>
+        <div className={styles.groupDiv33}>
+          <img
+            className={styles.rectangleIcon8}
+            alt=""
+            src="../rectangle-1210@2x.png"
+          />
+        </div>
+        <b className={styles.saintJhnB}>Saint Jhn</b>
+        <b className={styles.topArtistsB}>Top Artists</b>
+        <div className={styles.groupDiv34}>
+          <img
+            className={styles.rectangleIcon8}
+            alt=""
+            src="../rectangle-1211@2x.png"
+          />
+        </div>
+        <div className={styles.groupDiv35}>
+          <b className={styles.onyekaB}>Sex</b>
+          <b className={styles.b1}>01</b>
+          <div className={styles.teniDiv}>Trey</div>
+          <div className={styles.groupDiv36}>
+            <img
+              className={styles.rectangleIcon12}
+              alt=""
+              src="../rectangle-1212@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.groupDiv37}>
+          <b className={styles.onyekaB}>Onyeka</b>
+          <b className={styles.b1}>02</b>
+          <div className={styles.teniDiv}>Burna boy</div>
+          <div className={styles.groupDiv36}>
+            <img
+              className={styles.rectangleIcon12}
+              alt=""
+              src="../rectangle-1213@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.groupDiv39}>
+          <b className={styles.onyekaB}>Moslado</b>
+          <b className={styles.b1}>03</b>
+          <div className={styles.teniDiv}>Teni</div>
+          <div className={styles.groupDiv36}>
+            <img
+              className={styles.rectangleIcon12}
+              alt=""
+              src="../rectangle-1214@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.groupDiv41}>
+          <img
+            className={styles.rectangleIcon15}
+            alt=""
+            src="../rectangle-1215@2x.png"
+          />
+        </div>
+        <b className={styles.burnaBoy}>Burna boy</b>
+        <div className={styles.groupDiv42}>
+          <img
+            className={styles.rectangleIcon8}
+            alt=""
+            src="../rectangle-1216@2x.png"
+          />
+        </div>
+        <b className={styles.falzB}>Falz</b>
+        <div className={styles.groupDiv43}>
+          <img
+            className={styles.rectangleIcon8}
+            alt=""
+            src="../rectangle-1217@2x.png"
+          />
+        </div>
+        <img className={styles.groupIcon4} alt="" src="../group-36.svg" />
+        <div className={styles.groupDiv44}>
+          <div className={styles.groupDiv45}>
+            <img
+              className={styles.rectangleIcon18}
+              alt=""
+              src="../rectangle-1218@2x.png"
+            />
+          </div>
+          <div className={styles.rectangleDiv14} />
+        </div>
+        <div className={styles.groupDiv46}>
+          <img
+            className={styles.rectangleIcon19}
+            alt=""
+            src="../rectangle-1219@2x.png"
+          />
+        </div>
+        <b className={styles.sexB2}>Sex</b>
+        <div className={styles.treyDiv2}>Trey</div>
+        <img className={styles.groupIcon5} alt="" src="../group-40.svg" />
+        <img className={styles.frameIcon9} alt="" src="../frame9.svg" />
+        <b className={styles.nowPlayingB}>Now Playing</b>
+        <div className={styles.div}>2:20</div>
+        <div className={styles.groupDiv47}>
+          <img className={styles.frameIcon10} alt="" src="../frame10.svg" />
+          <div className={styles.div1}>2:20</div>
+        </div>
+        <div className={styles.groupDiv48}>
+          <img className={styles.frameIcon10} alt="" src="../frame11.svg" />
+          <div className={styles.div1}>3:20</div>
+        </div>
+        <div className={styles.groupDiv49}>
+          <img className={styles.frameIcon10} alt="" src="../frame12.svg" />
+          <div className={styles.div1}>3:29</div>
+        </div>
+        <div className={styles.div4}>3:33</div>
+        <img
+          className={styles.arrowRepeatAllNull}
+          alt=""
+          src="../arrow-repeat-all--null.svg"
+        />
+        <img className={styles.vectorIcon} alt="" src="../vector.svg" />
+        <img className={styles.vectorIcon1} alt="" src="../vector1.svg" />
+        <div className={styles.groupDiv50}>
+          <div className={styles.to33Users}>To 33 users</div>
+          <div className={styles.hoursDiv}>4 Hours</div>
+          <b className={styles.playlistSharedB}>Playlist shared</b>
+          <div className={styles.groupDiv51}>
+            <div className={styles.rectangleDiv15} />
+            <img
+              className={styles.iconlyBoldPaperPlus1}
+              alt=""
+              src="../iconlyboldpaper-plus1.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.groupDiv52}>
+          <div className={styles.to33Users}>26 songs</div>
+          <div className={styles.minDiv}>55 min</div>
+          <b className={styles.playlistSharedB}>Playlist added</b>
+          <div className={styles.groupDiv51}>
+            <div className={styles.rectangleDiv15} />
+            <img
+              className={styles.iconlyBoldPaperPlus1}
+              alt=""
+              src="../iconlyboldpaper-plus1.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FrameComponent;
